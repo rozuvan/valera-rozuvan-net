5608268a944c4ce11fd3a89363f041ad247f9c36
Valera Rozuvan <valera.rozuvan@gmail.com>
2017-08-17 12:36:11 +0300

    Updating site

---
+++ site_next/blog/code-highlighting-in-a-jekyll-based-blog/js/main.js
@@ -0,0 +1,40 @@
+/**
+ * "Rules of Optimization:
+ *     Rule 1: Don't do it.
+ *     Rule 2 (for experts only): Don't do it yet.”
+ *
+ * ~ Michael A. Jackson
+ */
+
+(function () {
+    var $, CodeMirror;
+
+    require.config({
+        baseUrl: 'js',
+        paths: {
+            jquery: '../../../js/vendor/jquery/jquery.min'
+        },
+        packages: [{
+            name: 'codemirror',
+            location: '../../../js/vendor/codemirror',
+            main: 'lib/codemirror'
+        }]
+    });
+
+    require(['jquery', 'codemirror', 'codemirror/mode/clike/clike'], function (_$, _CodeMirror) {
+        $ = _$;
+        CodeMirror = _CodeMirror;
+
+        $.noConflict();
+
+        init();
+    });
+
+    function init() {
+        editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+            mode: 'text/javascript',
+            lineNumbers: true,
+            readOnly: true
+        });
+    }
+}).call(this);
